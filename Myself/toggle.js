39d9e9a27e5ddf01900fdc14bcdd07f7fc579240
FolaBada <107040678+FolaBada@users.noreply.github.com>
2022-06-22 00:58:22 +0100

    Add files via upload

---
+++ Myself/toggle.js
@@ -0,0 +1,10 @@
+function toggle(){
+    var change = document.getElementById('div');
+
+    if(change.style.visibility === 'hidden'){
+        change.style.visibility = 'visible'
+    }
+ else{
+    change.style.visibility = 'hidden'
+ }
+}
